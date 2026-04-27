916727775d6a5f70079003f9fa41b189e6ad31ec
ldspyd <ldspyd1@gmail.com>
2026-04-27 14:36:06 -0600

    a

--- ponder/dom.js
+++ ponder/dom.js
@@ -1,3 +1,10 @@
+let title = document.querySelector('h1');
+
+console.log(title)
+
+title.textContent = 'Web Page Components';
+
+
 let selectElem = document.getElementById('webdevlist');
 selectElem.addEventListener('change', function(){
     let codeValue = selectElem.value;
