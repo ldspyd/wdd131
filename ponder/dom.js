3d24a70215cdf798378374a6275ca3386d730a51
ldspyd <ldspyd1@gmail.com>
2026-04-27 21:44:39 -0600

    a

--- ponder/dom.js
+++ ponder/dom.js
@@ -1,8 +1,27 @@
 let title = document.querySelector('h1');
-
 console.log(title)
-
 title.textContent = 'Web Page Components';
+// document.querySelector(('h1')).textContent = 'Web Page Components';
+
+// document.querySelector('#topics').style.color = 'red';
+
+const topics = document.querySelector('#topics');
+topics.style.color = 'purple';
+
+const wrapper = document.getElementById('content');
+wrapper.style.backgroundColor = 'lightblue';
+
+let list = document.querySelector('.list');
+// let list = document.querySelectorAll('.list');
+list.style.border = '3px solid black'
+
+let paragraph = document.querySelector('p');
+paragraph.style.fontSize = '3em';
+
+paragraph.classList.add('background');
+
+const image = document.querySelector('img');
+image.setAttribute('src', 'images/Web_Page_components.png');
 
 
 let selectElem = document.getElementById('webdevlist');
