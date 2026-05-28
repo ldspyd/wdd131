73bce22740c878bc718045feca5c2f1fc4d8df99
ldspyd <ldspyd1@gmail.com>
2026-05-27 21:33:00 -0600

    a

--- ponder/dynamic.js
+++ ponder/dynamic.js
@@ -37,12 +37,48 @@ const pets = ['cat', 'dog', 'bird']
 
 
 // constructor
-const animals = new Array['bear', 'lion']
+// const animals = new Array['bear', 'lion']
 
-pets.push('lizard');
+// pets.push('lizard');
 
-console.log(pets);
+// console.log(pets);
 
-console.log(pets[0]);
+// console.log(pets[0]);
 
-pets[3] = 'bunny'
+// pets[3] = 'bunny'
+
+
+// let fruit = ['watermelon', 'peach', 'apple', 'banana'];
+
+
+// fruit.push('orange');
+
+// console.log(fruit)
+
+// const fruit = ['watermelon','peach','apple'];
+// fruit.forEach(function(item){
+
+//      console.log(item);
+
+// });
+
+// const nums = [1, 2, 3, 4];
+// const dbNums = nums.map(function double(n){
+
+//    return n * 2;
+
+// });
+
+// console.log(dbNums);
+// console.log(nums);
+
+const nums = [1, 2, 3, 4];
+const sum = nums.reduce(function totalNums(total, item){
+
+           return total + item;
+
+});
+const avg = sum / 4;
+
+console.log(sum)
+console.log(avg)
